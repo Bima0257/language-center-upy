a476f9a3798bbd0afb0bee84af56f48b18f0b623
Bima0257 <bimatri377@gmail.com>
2026-08-20 08:34:53 +0700

    fix(sidebar): tambah overflow-y-auto agar menu bisa di-scroll

--- resources/js/Components/Dashboard/Sidebar.vue
+++ resources/js/Components/Dashboard/Sidebar.vue
@@ -119,7 +119,7 @@ watch(
             </div>
         </div>
 
-        <nav class="flex-1 space-y-2">
+        <nav class="flex-1 space-y-2 overflow-y-auto">
             <template v-for="item in nav" :key="item.label">
                 <div v-if="item.children">
                     <button
